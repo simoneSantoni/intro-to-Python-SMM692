5fd3c3a6edbf8c96bd431220ad80a0a1a1551354
simoneSantoni <simone.santoni.1@city.ac.uk>
2020-09-16 22:27:33 +0100

    Initial commit.

---
+++ _1/README.md
@@ -0,0 +1,9 @@
+Intro to Python, session #1 - README
+====================================
+
+This folder contains materials to be used for a sync (alive) session that aims
+to introduce the following points:
+
++ Python objects and their properties
++ iterators
+
---
+++ _1/python_objects.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- utf-8 -*-
+
+"""
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+
+Author   : Simone Santoni, simone.santoni.1@city.ac.uk
+
+Synopsis : intro to the fundamental types of Python objects
+
+Todos    :
+
+"""
+
+# %% load libraries
+import os
+
+# %% numbers
+# integers
+an_integer = 4
+
+
+# %% strings
+
+
+# %% dates
