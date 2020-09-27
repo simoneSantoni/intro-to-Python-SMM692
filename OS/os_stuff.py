fe21b03016a821f40070257fbbd850b49f2d77d6
simoneSantoni <simone.santoni.1@city.ac.uk>
2020-09-27 11:17:51 +0100

    Initial commit.

---
+++ OS/os_stuff.py
@@ -0,0 +1,17 @@
+#/usr/env/bin python3
+# -*- encoding utf-8 -*-
+
+"""
+-----------------------------------------------------------------------------
+    os_stuff.py  |  OS operations
+-----------------------------------------------------------------------------
+
+Auhor  : simone.santoni.1@city.ac.uk
+
+Agenda :
+
+    + reading and writing strings
+    + reading and writing lists
+    + reading and writing dictionaries
+
+"""
