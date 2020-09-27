1029259f5bed7d33a756e6748195446617df2e5a
simoneSantoni <simone.santoni.1@city.ac.uk>
2020-09-27 10:20:58 +0100

    Initial commit.

--- _2/while_loop.py
+++ _2/while_loop.py
@@ -1,7 +1,39 @@
+#/usr/env/bin python3
+# -*- encoding utf-8 -*-
 
+"""
+-----------------------------------------------------------------------------
+    while_loop.py  |  Control flow, iterators/loops
+-----------------------------------------------------------------------------
 
-Whereas a for loop is established for a fixed number of iterations, statements within the block of a while loop execute only and as long as some condition holds: >>> i = 0 >>> while i<10: ... i += 1 ...
-print(i, end=’.’)
-... >>> print()
-1.2.3.4.5.6.7.8.9.10.
-The counter i is initialized to 0,whichis lessthan10 so the while loop begins. On each iteration, i is incremented by one and its value printed. When i reaches 10,onthe following iteration i < 10 is False:the loop ends and execution continues afterthe loop,
+Auhor  : simone.santoni.1@city.ac.uk
+
+Agenda :
+
+    + what's a while loop?
+
+"""
+
+# %% what's a while loop?
+
+"""
+Whereas a for loop is established for a fixed number of iterations, statements
+within the block of a while loop execute only and as long as some condition
+holds (Hill, 2015).
+
+Possible application of the while loop:
+
+    - computer simulation of social and economic systems
+      - in fact, analysts are frequently interested in appreciating the
+        convergence of a system toward some equilibrium
+      - once the equilibrium has been achieved, the analyst wants to stop the
+        computer simulation
+"""
+
+# backbone of a while loop
+# --+ starting state
+i = 0
+# --+ while loop
+while i < 10:
+    i +=1                  # we update the starting state
+    print('The current state is {}'.format(i))
