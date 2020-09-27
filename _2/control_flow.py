6e178d5780c201522b6945a8f6db8d84d897a55b
simoneSantoni <simone.santoni.1@city.ac.uk>
2020-09-27 10:02:21 +0100

    Initial commit.

---
+++ _2/control_flow.py
@@ -0,0 +1,80 @@
+
+#/usr/env/bin python3
+# -*- encoding utf-8 -*-
+
+"""
+-----------------------------------------------------------------------------
+    control_flow.py  |  Control flow, iterators/loops
+-----------------------------------------------------------------------------
+
+Auhor  : simone.santoni.1@city.ac.uk
+
+Agenda :
+
+    + the if statement
+    + conditional foor loops ― examples
+
+"""
+
+# %% the if statements
+
+"""
+Few computer programs are executed in a purely linear fashion, one
+statement after another as written in the source code. It is more likely that
+during the program execution, data objects are inspected and blocks of code
+executed conditionally on the basis of some test carried out on them. Thus,
+all practical languages have the equivalent of an if-then-(else) construction.
+(Hill, 2015)
+"""
+
+# %% conditional for loops ― examples
+
+# sample data
+# --+ columns are company, sector, stock market
+company_data = [['a', 'finacial services', 'UK'],
+                ['b', 'automotive', 'DE']]
+# --+ data are nested within companies; columns are company, year, return on
+#   equity (%)
+profitability = [['a', 2018, 5.2],
+                 ['b', 2018, 4.3],
+                 ['a', 2019, 5.8],
+                 ['b', 2019, 4.6]]
+
+# example 1: merge company data and profitability for the most recent year
+merge = []
+for i in company_data:
+    for j in profitability:
+        if i[0] == j[0]:               # match data with respect to comapnies
+            to_append = i + j[1:]
+            merge.append(to_append)
+        else:
+            pass                       # do nothing
+
+merge
+
+# example 2: merge company data and profitability for the most recent year
+merge = []
+for i in company_data:
+    for j in profitability:
+        if i[0] == j[0]:               # match data with respect to comapnies
+            if j[1] == 2019:           # retain most recent data
+                to_append = i + j[1:]
+                merge.append(to_append)
+            else:
+                pass                   # do nothing
+        else:
+            pass
+
+merge
+
+# example 3: redo 2 by avoiding nested if statements
+merge = []
+for i in company_data:
+    for j in profitability:
+        if (i[0] == j[0]) & (j[1] == 2019): # match data on company and year
+            to_append = i + j[1:]
+            merge.append(to_append)
+        else:
+            pass
+
+merge
--- _2/for_loop.py
+++ _2/for_loop.py
@@ -32,6 +32,67 @@ and convenient way is with the idiom:
 """
 
 # backbone of a foor loop
-# --+ we need an iterable object
-l = ['Apple', 'Microsoft', 'Lenovo', 'Intel']
-# --+ let's print out the names of these companies one by one
+# --+ we need an iterable object ― e.g., a list of company names
+companies = ['Apple', 'Microsoft', 'Lenovo', 'Intel']
+# --+ task to repeat: let's print out each company name separately
+for company in companies:
+    print(company)
+
+# %% examples
+
+# example 1: get the z-scores of an observed distribution
+# --+ the distribution
+import numpy as np
+d = np.random.poisson(lam=6, size=100)
+d
+# --+ get mean and standard deviation
+mu, std = np.mean(d), np.std(d)
+# --+ empty container
+z = []
+for i in d:
+    to_append = (i - mu) / std
+    z.append(to_append)
+
+z
+
+# example 2: cleaning a list with string data, display the result, and provide
+#            details on the counts of changes made on each element of the list
+# --+ US cities with leading and trailing blanks
+cities = [' New York', 'San Fran', 'Boston ', ' Chicago ']
+# --+ let's display the clean string
+for city in cities:
+    clean_str = city.strip()
+    print('''
+    -------------------------------
+    City: {}
+
+    Counts of blanks removed : {}
+    '''.format(clean_str, len(city) - len(clean_str)))
+
+# example 3: create all possible permutations involving two sets and store
+#            them in a new list
+# --+ data
+l_0 = [1, 2, 3]
+l_1 = ['a', 'b', 'c']
+# --+ container
+output = []
+# --+ nested for loop
+for i in l_0:
+    for j in l_1:
+        to_append = [i, j]
+        output.append(to_append)
+
+output
+
+
+# example 4: match the elements from two lists that occupy the same position
+#            (i.e., have same index) and store them in a new list
+# --+ container
+output = []
+# --+ now, the foor loop includes the generator 'zip' to create a 1-to-1
+#     matching
+for i, j in zip(l_0, l_1):
+    to_append = [i, j]
+    output.append(to_append)
+
+output
