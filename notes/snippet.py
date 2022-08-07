4726b4d53c2623b4f5512106ee9e0323cebd9010
Simone Santoni <simone.santoni.1@city.ac.uk>
2022-08-07 12:05:39 +0100

    Minor edits

---
+++ notes/snippet.py
@@ -0,0 +1,10 @@
+# %%
+# load modules
+import numpy as np
+
+# %%
+# array-creating routines
+np.zeros((4,4))
+np.ones((4,4))
+np.full((4,4,), -99)
+np.identity(4)
--- notes/test.py
+++
@@ -1,6 +0,0 @@
-import os
-import glob
-import numpy as np
-
-# %%
-# 
