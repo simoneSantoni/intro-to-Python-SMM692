52b0738333622c3bf5f5809904ee0869e6620a33
Simone Santoni <simone.santoni.1@city.ac.uk>
2022-08-08 08:35:09 +0100

    Minor edits

--- notes/snippet.py
+++ notes/snippet.py
@@ -3,8 +3,30 @@
 import numpy as np
 
 # %%
-# array-creating routines
-np.zeros((4,4))
-np.ones((4,4))
-np.full((4,4,), -99)
-np.identity(4)
+# get data from a text file
+# --+ create a string with the data and some qualitative comments on them
+S = """
+# Below are some demographic data about Michael J. Jordan (basketball player)
+# from Wikipedia.
+# 
+# Data labels are:
+# 
+# NAME, BORN, NBA CHAMPIONSHIPS, AVERAGE POINT PER GAME
+"Jordan, Michael Jeffrey","February 17, 1963",6,30.1
+"""
+# --+ write the data to a file
+with open("my_data", "w") as pipe:
+    pipe.write(S)
+pipe.close()
+
+np.loadtxt(
+    open("my_data", "r"),
+    dtype={
+        "labels": ("NAME", "BORN", "NBA CHAMPIONSHIPS", "AVERAGE POINT PER GAME"),
+        "formats": ("S1", "S1", "i4", "i4"),
+    },
+    comments="#",
+    delimiter=',',
+    quotechar='"'
+)
+
