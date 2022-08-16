5ce0c3a38fb0589318f9523cc154b0b99d0a80d6
Simone Santoni <simone.santoni.1@city.ac.uk>
2022-08-16 20:52:14 +0100

    Minor edits

--- notes/snippet.py
+++ notes/snippet.py
@@ -1,29 +1,45 @@
 # %%
 # load modules
+from os import P_PIDFD
 from tkinter import N
 import numpy as np
 import matplotlib.pyplot as plt
-
 from collections import Counter
 
 # %%
 
-N = 
+N = np.random.normal(loc=10, scale=10, size=10000) 
+P = np.random.poisson(lam=10, size=10000) 
 
+# %%
+# plot histogram
 
 fig = plt.figure(figsize=(3, 3))
 ax = fig.add_subplot(111)
-ax.plot(x, y, 'o', label='Observed data', color='gray', markersize=5)
-ax.plot(x, b[1]*x + b[0], 'blue', label='OLS estimate')
-ax.set_xticks([0, 5, 10, 15])
-ax.set_xticklabels(['0', '5', '10', '15'])
-ax.set_xlabel('$x$')
-ax.set_ylabel('$y$')
-plt.legend()
+ax.hist(N, color='red')
+ax.set_ylabel('Count')
+ax.set_xlabel('Value')
+plt.title('Normal distribution data')
 plt.grid(True)
 plt.savefig(
-    "ols.pgf",
+    "norm.pgf",
     transparent=True,
     bbox_inches="tight",
     pad_inches=0,
 )
+# %%
+P_FR = Counter(P)
+fig = plt.figure(figsize=(3, 3))
+ax = fig.add_subplot(111)
+ax.scatter(P_FR.keys(), P_FR.values(), color='blue')
+ax.set_ylabel('Count')
+ax.set_xlabel('Value')
+plt.title('Poisson distribution data')
+plt.grid(True)
+plt.savefig(
+    "poisson.pgf",
+    transparent=True,
+    bbox_inches="tight",
+    pad_inches=0,
+)
+# %%
