974ea3fd745143a5fc06acc475f5f05c6e656868
Simone Santoni <simone.santoni.1@city.ac.uk>
2022-08-20 15:56:29 +0100

    Minor edits

--- notes/snippet.py
+++ notes/snippet.py
@@ -1,14 +1,9 @@
 import pandas as pd
-
 df = pd.DataFrame.from_dict(
     {
-        "timestamp": [
-            "2017-10-08 12:03:05",
-            "2020-09-07 08:09:45",
-            "2021-04-11 10:12:13",
-        ],
-        "product": ["Darth Vader plush", "Obi-Wan Kenobi lightsaber", "Yoda pijamas"],
-        "reviewer": ["Sheldon", "Sheldon", "Leonard"],
-        "rating": [1, 2, 5],
+        "product": ["a", "b", "c"],
+        "price": [9.87, 8.63, 6.45],
+        "colror": ["green", "green", "blue"],
     }
 )
+
