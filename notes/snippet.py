cc1515b2aa2048ac07d4aaa077340d5878ee818d
Simone Santoni <simone.santoni.1@city.ac.uk>
2022-08-22 00:13:56 +0100

    Minor edits

--- notes/snippet.py
+++ notes/snippet.py
@@ -1,9 +1,2 @@
-import pandas as pd
-df = pd.DataFrame.from_dict(
-    {
-        "product": ["a", "b", "c"],
-        "price": [9.87, 8.63, 6.45],
-        "colror": ["green", "green", "blue"],
-    }
-)
 
+gr.aggregate([np.sum, np.mean, np.std])
